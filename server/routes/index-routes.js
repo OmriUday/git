51797adebdcd7c7c66c25aa8b51ca7b937335cc0
Omri Uday <93121703+OmriUday@users.noreply.github.com>
2022-06-13 16:01:44 +0300

    Delete server directory

--- server/routes/index-routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const indexController = require('../controllers/index-controller');
-
-router.get('/:searchText', indexController.getSearchResult);
-
-module.exports = router;
