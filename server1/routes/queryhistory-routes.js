2c4718d07669c9c67055821b1af33b78eed30694
Omri Uday <93121703+OmriUday@users.noreply.github.com>
2022-06-22 16:48:47 +0300

    Delete server1 directory

--- server1/routes/queryhistory-routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const queryController = require('../controllers/queryHistory-controller');
-
-router.post('/', queryController.saveQueryHistory);
-router.get('/', queryController.getQueryHistory);
-
-module.exports = router;
